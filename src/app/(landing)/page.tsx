8d83e509142e50bd80af44df1de8874a903d7bce
sajib689 <jibonraja11@gmail.com>
2024-12-19 21:02:14 +0600

    add recommend components on the home page

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/common/Navbar";
 import Banner from "@/components/home/Banner";
+import Recommend from "@/components/home/Recommend";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <div className="font-[family-name:var(--font-geist-sans)]">
      <Navbar/>
      <Banner/>
+     <Recommend/>
     </div>
   );
 }
