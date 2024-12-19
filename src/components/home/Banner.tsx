d957b4ef41a5fef1b2b2cd96a8d570a4bf88509d
sajib689 <jibonraja11@gmail.com>
2024-12-19 17:13:49 +0600

    add the banner

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -3,11 +3,11 @@ const Banner = () => {
       <div
         style={{
           backgroundImage: `url('/assets/images/bg-banner.png')`,
-          backgroundSize: "cover", // Ensures the image covers the container
-          backgroundPosition: "center", // Centers the image
-          backgroundRepeat: "no-repeat", // Prevents tiling
-          height: "400px", // Adjust as needed
-          width: "100%", // Makes it span the full width of the container
+          backgroundSize: "cover", 
+          backgroundPosition: "center", 
+          backgroundRepeat: "no-repeat",
+          height: "85vh", 
+          width: "100%", 
         }}
       ></div>
     );
