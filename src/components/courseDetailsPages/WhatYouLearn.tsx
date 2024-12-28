68935c243f14f66ffeb790b68c1ffd520f5ec477
sajib689 <jibonraja11@gmail.com>
2024-12-28 18:47:01 +0600

    add the title

--- src/components/courseDetailsPages/WhatYouLearn.tsx
+++ src/components/courseDetailsPages/WhatYouLearn.tsx
@@ -1,21 +1,34 @@
-'use client'
+"use client";
 
-import { Box } from "@mui/material";
+import { Box,Typography } from "@mui/material";
 
 const WhatYouLearn = () => {
-    return (
-        <Box  sx={{
-            backgroundColor: "#B0EACD", 
-            color: "black",
-            textAlign: "center",
-            py: 4,
-            px: 2,
-            maxWidth: "100%",
-            mx: "auto",
-          }}>
-            
-        </Box>
-    );
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#B0EACD",
+        color: "black",
+        textAlign: "center",
+        py: 4,
+        px: 2,
+        maxWidth: "100%",
+        mx: "auto",
+      }}
+    >
+      {/* Title */}
+      <Typography
+        variant="h4"
+        sx={{
+          fontWeight: 700,
+          mb: 4,
+          fontSize: "2rem",
+        }}
+      >
+        What you will learn_ from this course?
+
+      </Typography>
+    </Box>
+  );
 };
 
 export default WhatYouLearn;
