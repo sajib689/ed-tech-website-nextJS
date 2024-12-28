f5fd1aaddb67234e18f83bc7f8f69a7319000530
sajib689 <jibonraja11@gmail.com>
2024-12-28 18:44:11 +0600

    add a new components about what you learn from this course

---
+++ src/components/courseDetailsPages/WhatYouLearn.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { Box } from "@mui/material";
+
+const WhatYouLearn = () => {
+    return (
+        <Box  sx={{
+            backgroundColor: "#B0EACD", 
+            color: "black",
+            textAlign: "center",
+            py: 4,
+            px: 2,
+            maxWidth: "100%",
+            mx: "auto",
+          }}>
+            
+        </Box>
+    );
+};
+
+export default WhatYouLearn;
