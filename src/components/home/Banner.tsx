64bde9d5b0353b34ab163afe9ed16fb3e00cc762
sajib689 <jibonraja11@gmail.com>
2025-01-21 20:55:05 +0600

    feat: enhance Banner component with improved styles and gradient text

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { Typography, Box } from "@mui/material";
 import React, { useEffect } from "react";
 import AOS from "aos";
@@ -6,37 +6,37 @@ import "aos/dist/aos.css";
 const Banner = () => {
   useEffect(() => {
     AOS.init({
-      duration: 500, 
-      once: true, 
+      duration: 500,
+      once: true,
     });
   }, []);
   return (
     <Box
-    data-aos="fade-right"
+      data-aos="fade-right"
       sx={{
         position: "relative",
         backgroundImage: `url('/assets/images/bg-banner.png')`,
         backgroundSize: "cover",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
-        height: "calc(100vh - 64px)", 
+        height: "calc(100vh - 64px)",
         width: "100%",
         display: "flex",
         alignItems: "center",
         justifyContent: "flex-end",
-        paddingRight: "20px", 
+        paddingRight: "20px",
         overflow: "hidden",
         boxSizing: "border-box",
-       
+
         "@media (max-width: 768px)": {
-          backgroundPosition: "center", 
-          height: "auto", 
+          backgroundPosition: "center",
+          height: "auto",
           paddingRight: "15px",
         },
       }}
     >
       <Box
-      data-aos="fade-right"
+        data-aos="fade-right"
         sx={{
           zIndex: 1,
           textAlign: "right",
@@ -44,8 +44,8 @@ const Banner = () => {
           paddingRight: "20px",
           boxSizing: "border-box",
           "@media (max-width: 768px)": {
-            textAlign: "center", 
-            paddingRight: "15px", 
+            textAlign: "center",
+            paddingRight: "15px",
           },
         }}
       >
@@ -53,23 +53,27 @@ const Banner = () => {
           variant="h1"
           sx={{
             fontWeight: 700,
-            color: "#21BF73",
             fontSize: { xs: "2.5rem", sm: "6rem" },
+            background: "linear-gradient(to right, #6a11cb, #2575fc)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
           }}
         >
           Learn Code
         </Typography>
+
         <Typography
           variant="h4"
           sx={{
             mt: 2,
-            fontSize: { xs: "1rem", sm: "1.25rem" }, 
+            fontSize: { xs: "1rem", sm: "1.25rem" },
             textAlign: { xs: "center", sm: "right" },
           }}
         >
-          If you don&apos;t want to do it, &quot;loss&quot;?
-          Hold <br /> onto the to-do list and become the <br />
-          &quot;boss.&quot; Learn with confidence, <br /> success is in your search.
+          If you don&apos;t want to do it, &quot;loss&quot;? Hold <br /> onto
+          the to-do list and become the <br />
+          &quot;boss.&quot; Learn with confidence, <br /> success is in your
+          search.
         </Typography>
       </Box>
     </Box>
