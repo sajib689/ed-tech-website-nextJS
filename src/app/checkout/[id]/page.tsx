b3ffd53161fe0ba80e852d0ddc8f252c6275c4bb
sajib689 <jibonraja11@gmail.com>
2025-01-21 21:03:10 +0600

    feat: implement course fetching in Checkout component with loading and error handling

--- src/app/checkout/[id]/page.tsx
+++ src/app/checkout/[id]/page.tsx
@@ -3,6 +3,7 @@ import Checkout from "@/components/course/Checkout";
 
 export default function page({ params }: { params: { id: string } }) {
   const { id } = params;
+  console.log(id);
   return (
     <div>
       <Checkout id={id}/>
