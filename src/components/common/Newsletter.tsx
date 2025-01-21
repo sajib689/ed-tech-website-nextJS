c36b7e711e3d6dc1a39282fb34fca95cb7f3befc
sajib689 <jibonraja11@gmail.com>
2025-01-21 20:40:22 +0600

    feat: update Footer and Newsletter components with gradient backgrounds and improved styles

--- src/components/common/Newsletter.tsx
+++ src/components/common/Newsletter.tsx
@@ -53,13 +53,17 @@ const Newsletter = () => {
         variant="contained"
         sx={{
           color: "#fff",
-          backgroundColor: "#21BF73",
+          fontWeight: "bold",
+          background: "linear-gradient(to right, #6a11cb, #2575fc)",
+
           borderRadius: "0 50px 50px 0",
           padding: "8px 16px", // Reduced padding for smaller width
           fontSize: "1rem",
           transition: "all 0.3s ease",
           "&:hover": {
-            backgroundColor: "#1e9e5f",
+            fontWeight: "bold",
+            background: "linear-gradient(to right, #6a11cb, #2575fc)",
+
             transform: "scale(1.05)",
           },
         }}
