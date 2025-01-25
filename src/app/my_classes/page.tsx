0c0d41ee2461f7aac7ad0418ff60ebc284bd266b
sajib689 <jibonraja11@gmail.com>
2025-01-25 20:32:22 +0600

    feat: integrate MyClass component into my_classes page

--- src/app/my_classes/page.tsx
+++ src/app/my_classes/page.tsx
@@ -1,7 +1,10 @@
+import MyClass from '@/components/myclasses/MyClass'
 import React from 'react'
 
 export default function my_classes() {
   return (
-    <div>my_classes</div>
+    <div>
+      <MyClass/>
+    </div>
   )
 }
