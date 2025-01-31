b7e14294c33961242188455a974fcf39293b93dd
sajib689 <jibonraja11@gmail.com>
2025-01-31 17:19:34 +0600

    feat: enhance UserProfile component with authentication context and loading state

--- src/components/profile/UserProfile.tsx
+++ src/components/profile/UserProfile.tsx
@@ -1,5 +1,17 @@
 'use client'
+
+import { AuthContext } from "@/context/AuthProvider";
+import useUserByEmail from "@/hooks/useUserByEmail";
+import Loader from "@/utlis/Loader";
+import { useContext } from "react";
+
 const UserProfile = () => {
+    const authContext = useContext(AuthContext)
+    if(!authContext){
+        <Loader/>
+    }
+    const {user} = authContext
+    const { userData, loadings, errors } = useUserByEmail(user?.email)
     return (
         <div>
             
